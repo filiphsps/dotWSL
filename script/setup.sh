474d1d026118f1c6c99efb5eb68e41f16280ae66
Filiph Siitam Sandström <filiph@nordcom.io>
2023-10-29 11:48:30 +0100

    Setup: Run script based on env.

--- script/setup.sh
+++ script/setup.sh
@@ -1,1 +1,33 @@
 #!/bin/sh
+
+set -eu
+
+. ./common.sh
+
+check_for_bash
+check_os
+
+if [ "$HAS_BASH" ]; then
+    set -o pipefail
+fi
+
+case "$OS" in
+    "macOS")
+        ./scripts/install/install-macos.sh
+    ;;
+    "Debian Linux")
+        ./scripts/install/install-debian.sh
+    ;;
+    "Debian WSL")
+        ./scripts/install/install-debian.sh
+    ;;
+    "Ubuntu Linux")
+        ./scripts/install/install-ubuntu.sh
+    ;;
+    "Ubuntu WSL")
+        ./scripts/install/install-ubuntu.sh
+    ;;
+    "Alpine Linux")
+        ./scripts/install/install-alpine.sh
+    ;;
+esac
